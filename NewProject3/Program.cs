988ea518055133d67a892a15fbb02111b10adcd7
Aleksey <a.y.zayitsev@gmail.com>
2023-01-07 22:57:44 +0300

    newProject сложени рандомных чисел

---
+++ NewProject3/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random ().Next (1, 50);
+Console.Write (numberA);
+int numberB = new Random ().Next (1, 40);
+Console.Write (" + ");
+Console.Write (numberB);
+int result = numberA + numberB;
+Console.Write (" = ");
+Console.Write (result);
