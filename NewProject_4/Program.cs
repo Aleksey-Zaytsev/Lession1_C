5c4b44c68f2da11789c68804fa41e4f5e40fdbce
Aleksey <a.y.zayitsev@gmail.com>
2023-01-07 23:44:53 +0300

    newProject особое приветствие

---
+++ NewProject_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Ввидите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if ( username.ToLower() == "оксана" )
+{
+    Console.WriteLine ("Приветик моя дорогая Оксаночка :*");
+}
+else
+{
+    Console.Write ("Привет ");
+    Console.WriteLine (username);
+}
